31926ead0eb06377f4167b88e4e0f3d94359007c
Vishu4355 <vish44harem@gmail.com>
2026-07-21 18:22:59 +0530

    Created a simple Background

---
+++ magic.js
@@ -0,0 +1,215 @@
+console.log("magic.js loaded");
+
+
+
+
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('mycan1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1200;
+    canvas.height = 730;
+
+    class Player {
+
+        constructor(game){
+            this.game = game;
+            this.Collisionx;
+            this.Collisionxy;
+
+        }
+        draw(context){
+            context.beginPath();
+            context.arc(100,100,50,0, Math.PI*2);
+            context.fill();
+        }
+
+    
+        
+
+    }
+
+
+    class Background {
+
+        constructor(game) {
+            this.game = game;
+
+            this.skyimg = new Image();
+            this.skyimg.src = "assets/Multi_Platformer_Tileset_Free/GrassLand/Background/GrassLand_Background_1.png"
+
+            this.skyimg.onload = () => {
+            console.log("Sky loaded");
+            console.log(this.skyimg.naturalWidth, this.skyimg.naturalHeight);
+            this.game.render(ctx);
+            };
+
+           
+            this.gravel = new Image();
+            this.gravel.src = "assets/Multi_Platformer_Tileset_Free/GrassLand/Background/GrassLand_Background_2.png"
+
+            this.gravel.onload = () => {
+            console.log("Sky loaded");
+            console.log(this.gravel.naturalWidth, this.gravel.naturalHeight);
+            this.game.render(ctx);
+            };
+
+            this.plain = new Image();
+            this.plain.src = "assets/Multi_Platformer_Tileset_Free/GrassLand/Background/GrassLand_Background_3.png"
+
+            this.plain.onload = () => {
+            console.log("Sky loaded");
+            console.log(this.plain.naturalWidth, this.plain.naturalHeight);
+            this.game.render(ctx);
+            };
+
+            this.grass = new Image();
+            this.grass.src = "assets/Multi_Platformer_Tileset_Free/GrassLand/Background/GrassLand_Background_4.png"
+
+            this.grass.onload = () => {
+            console.log("Sky loaded");
+            console.log(this.grass.naturalWidth, this.grass.naturalHeight);
+            this.game.render(ctx);
+            };
+
+            this.undergrass = new Image();
+            this.undergrass.src = "assets/Multi_Platformer_Tileset_Free/GrassLand/Background/GrassLand_Background_5.png"
+
+            this.undergrass.onload = () => {
+            console.log("Sky loaded");
+            console.log(this.undergrass.naturalWidth, this.undergrass.naturalHeight);
+            this.game.render(ctx);
+            };
+
+            // Height
+
+
+            this.gravelHeight = 650;
+            this.plainHeight = 670;
+            this.grassHeight = 680;
+            this.undergrassHeight = 500;
+
+
+            // Overlap 
+
+            const Overlap = 40;
+
+            // Position
+
+            this.undergrassY = this.game.height - this.undergrassHeight;
+            this.grassY = this.game.height - this.grassHeight + Overlap;
+            this.plainY = this.game.height  - this.plainHeight + Overlap;
+            this.gravelY = this.game.height - this.gravelHeight + Overlap;
+
+
+
+
+
+
+        }
+
+        draw(context) {
+
+
+
+            context.drawImage(this.skyimg,0,0,this.game.width, this.game.height);
+
+
+
+            const gravelTileWidth = this.gravel.naturalWidth || 416;
+            for(let x = 0; x< this.game.width; x += gravelTileWidth) {
+                context.drawImage(this.gravel, x, this.gravelY, gravelTileWidth,this.gravelHeight);
+            }
+
+
+            const plainTileWidth = this.plain.naturalWidth || 368;
+            for(let x = 0; x< this.game.width; x += plainTileWidth) {
+                context.drawImage(this.plain, x, this.plainY, plainTileWidth,this.plainHeight);
+            }
+
+
+            
+            const grassTileWidth = this.grass.naturalWidth || 448;
+            for(let x = 0; x< this.game.width; x += grassTileWidth) {
+                context.drawImage(this.grass, x, this.grassY, grassTileWidth,this.grassHeight);
+            }
+
+
+
+
+
+            const undergrassTileWidth = this.undergrass.naturalWidth || 368;
+            for(let x = 0; x< this.game.width; x += undergrassTileWidth) {
+                context.drawImage(this.undergrass, x, this.undergrassY, undergrassTileWidth,this.undergrassHeight);
+            }
+
+
+
+
+        }
+
+
+    }
+
+
+
+    class Game {
+        constructor(canvas){
+            this.canvas = canvas;
+            this.width = this.canvas.width;
+            this.height = this.canvas.height;
+            this.background = new Background(this);
+            this.Player = new Player(this);
+            this.mouse = {
+                x: this.width * 0.5,
+                y: this.height * 0.5,
+                pressed: false
+            }
+
+
+            canvas.addEventListener('mousedown', (e) =>{
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = true;
+
+            });
+
+            
+            canvas.addEventListener('mouseup', (e) =>{
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = false;
+
+            });
+
+            
+            canvas.addEventListener('mousemove', (e) =>{
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = true;
+
+            });
+
+        }
+
+        render(context) {
+            this.background.draw(context);
+            this.Player.draw(context);
+        }
+    }
+
+    const game = new Game(canvas);
+    game.render(ctx);
+   
+
+
+
+
+    
+    
+});
+
+
+console.log("JS file loaded");
+
+
+
